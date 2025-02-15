c4c46dab293fd7daddbd49919f70737bffc72964
HodeonArtz <martidicianolold@gmail.com>
2025-02-15 13:07:23 +0100

    perf: add babel compiler for optimized rerendering

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
